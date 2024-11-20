4848666bb20e05ad5b4da07f9929c91575b1e3d3
Roderick Lan <rodericklan03@gmail.com>
2024-11-19 21:06:32 -0700

    tighter loop

--- BasicSc2BotUtils.cpp
+++ BasicSc2BotUtils.cpp
@@ -332,7 +332,8 @@ sc2::Point2D BasicSc2Bot::FindPlaceablePositionNear(const sc2::Point2D& starting
         y_lo -= y_step;
         x_hi += y_step;
         
-        if (loop_count++ > 10) {
+        // if (loop_count++ > 10) {
+        if (loop_count++ > 5) {
             std::cout << "LOTS OF LOOPS OOPS " << loop_count << std::endl;
             return sc2::Point2D(0, 0);
             /*
