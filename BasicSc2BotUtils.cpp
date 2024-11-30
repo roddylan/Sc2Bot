799f371a805351ab16fb21c873cc984f63b34d93
Maurius Kritzinger <maurius@ualberta.ca>
2024-11-30 13:40:35 -0700

    remove debug statements

--- BasicSc2BotUtils.cpp
+++ BasicSc2BotUtils.cpp
@@ -368,7 +368,6 @@ sc2::Point2D BasicSc2Bot::FindPlaceablePositionNear(const sc2::Point2D& starting
 
                     // Stop searching if we cannot find a point before threshold -> use old way
                     if (direction_changes > 4) {
-                        std::cout << "Unable to find placement point" << std::endl;
                         new_point = sc2::Point2D(0, 0); 
                         break;
                     }
