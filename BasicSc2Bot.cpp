fe7c2365846f5a164d93b991f733bbe27295a332
Maurius Kritzinger <maurius@ualberta.ca>
2024-11-29 22:31:47 -0700

    bug fixes

--- BasicSc2Bot.cpp
+++ BasicSc2Bot.cpp
@@ -144,10 +144,12 @@ void BasicSc2Bot::OnStep() {
     // if (TryBuildMissileTurret()) {
     //     return;
     // }
-    if (obs->GetMinerals() - 100 >= 400 || obs->GetFoodUsed() - obs->GetFoodCap() < 30) {
+    /*
+    if (obs->GetMinerals() - 100 >= 400 || current_supply_use < ((1 / 2) * obs->get)) {
         TryBuildSupplyDepot();
         return;
-    }    
+    } 
+    */
     
     
 
@@ -273,7 +275,7 @@ void BasicSc2Bot::OnUnitDestroyed(const sc2::Unit* unit) {
             Actions()->UnitCommand(marine, sc2::ABILITY_ID::SMART, unit->pos);
         }
         */
-        if (Observation()->GetGameLoop() < 13200 && Distance2D(unit->pos, start_location) > 50.0f && unit->unit_type != sc2::UNIT_TYPEID::TERRAN_SCV) {
+        if ((Observation()->GetGameLoop() < 13200 && Distance2D(unit->pos, start_location) > 50.0f) && unit != this->scout) {
             return;
         }
         sc2::Units vikings = Observation()->GetUnits(sc2::Unit::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_VIKINGFIGHTER));
