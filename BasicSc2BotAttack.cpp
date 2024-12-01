f84b772c0bc93cd81a4fb942fadb6c255fe34cb0
Roderick Lan <rodericklan03@gmail.com>
2024-12-01 02:46:12 -0700

    Revert "fixed bound for sendsquad"
    This reverts commit 46439a8cdc22f299c0c5bc2041e8911621426981.
    
    catchup

--- BasicSc2BotAttack.cpp
+++ BasicSc2BotAttack.cpp
@@ -139,8 +139,7 @@ void BasicSc2Bot::SendSquad() {
     const uint64_t minute = 1344;
     // Dont continue if less than 13 mins has elapsed (build order not ready yet)
     // if (Observation()->GetGameLoop() < 13 * minute) {
-    // if (Observation()->GetFoodUsed() > ATTACK_FOOD) {
-    if (Observation()->GetFoodUsed() < ATTACK_FOOD) {
+    if (Observation()->GetFoodUsed() > ATTACK_FOOD) {
         return;
     }
     std::vector<sc2::Point2D> enemy_locations = Observation()->GetGameInfo().enemy_start_locations;
