b53b79dd98d56d4b2807f8112f41021ff2cea9e5
Maurius Kritzinger <maurius@ualberta.ca>
2024-12-01 19:06:41 -0700

    dancing gone? + speedup

--- BasicSc2Bot.cpp
+++ BasicSc2Bot.cpp
@@ -139,13 +139,13 @@ void BasicSc2Bot::OnStep() {
     }
     */
     // SendSquad();
-    LaunchAttack(); // TODO: fix implementation for final attack logic
+   // LaunchAttack(); // TODO: fix implementation for final attack logic
     HandleBuild();
     BuildWorkers();
     RecheckUnitIdle();
 
     CheckScoutStatus();
-    AttackIntruders();
+   // AttackIntruders();
 
     // BuildArmy(); // TODO: use this
     
@@ -488,18 +488,20 @@ void BasicSc2Bot::OnUnitIdle(const sc2::Unit* unit) {
         AssignEngineeringBayAction(*unit);
         break;
     }
-    case sc2::UNIT_TYPEID::TERRAN_MARINE: {
+         
+    //case sc2::UNIT_TYPEID::TERRAN_MARINE: {
         // if the bunkers are full
 
-        if (!LoadBunker(unit)) {
-            const sc2::GameInfo& game_info = Observation()->GetGameInfo();
+       // if (!LoadBunker(unit)) {
+         //   const sc2::GameInfo& game_info = Observation()->GetGameInfo();
             /*Actions()->UnitCommand(unit, sc2::ABILITY_ID::ATTACK_ATTACK
                 , game_info.enemy_start_locations.front(), true);*/
             // std::cout << "sent";
-        }
+      //  }
 
-        break;
-    }
+     //   break;
+  // }
+
     case sc2::UNIT_TYPEID::TERRAN_FACTORY: {
         UpgradeFactoryTechLab(unit);
         // AssignFactoryAction(unit);
