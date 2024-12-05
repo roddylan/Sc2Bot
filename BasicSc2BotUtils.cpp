ec7124005e10f492cbd827cd68190542bf676f7a
Maurius Kritzinger <maurius@ualberta.ca>
2024-12-04 20:35:34 -0700

    commented utils

--- BasicSc2BotUtils.cpp
+++ BasicSc2BotUtils.cpp
@@ -15,15 +15,23 @@
 #include <sc2lib/sc2_search.h>
 #include <iostream>
 #include <cmath>
-
+/*
+ * Enemy Filter
+ */
 sc2::Filter isEnemy = [](const sc2::Unit& unit) {
     return unit.alliance != sc2::Unit::Alliance::Self; 
 };
-
+/*
+ * @brief InBunker
+ *
+ * @param myUnits
+ * @param marine
+ * @return bool
+ */
 bool InBunker (sc2::Units myUnits, const sc2::Unit* marine) {
     for (auto& bunker : myUnits) {
         for (const auto& passenger : bunker->passengers) {
-            // check if already in bunker
+            // Check if already in bunker
             if (passenger.tag == marine->tag) {
                 return true;
             }
@@ -31,13 +39,18 @@ bool InBunker (sc2::Units myUnits, const sc2::Unit* marine) {
     }
     return false;
 }
-
+/*
+ * @brief Loads marine into a bunker
+ *
+ * @param marine
+ * @return bool
+ */
 bool BasicSc2Bot::LoadBunker(const sc2::Unit* marine) {
     sc2::Units myUnits = Observation()->GetUnits(sc2::Unit::Alliance::Self, IsUnit(sc2::UNIT_TYPEID::TERRAN_BUNKER));
     bool bunkerLoaded = false;
-
+    // If already in bunker 
     if (InBunker(myUnits, marine)) return true;
-
+    // Load bunkers
     for (auto target : myUnits) {
         if (target->passengers.size() < 4) {
             Actions()->UnitCommand(marine, sc2::ABILITY_ID::SMART, target);
@@ -49,10 +62,16 @@ bool BasicSc2Bot::LoadBunker(const sc2::Unit* marine) {
 
     return bunkerLoaded;
 }
-
+/*
+ * @brief Counts how many siege tanks are near factory
+ *
+ * @param factory
+ * @return int
+ */
 int BasicSc2Bot::CountNearbySeigeTanks(const sc2::Unit* factory) {
     sc2::Units seige_tanks = Observation()->GetUnits(sc2::Unit::Alliance::Self, IsUnit(sc2::UNIT_TYPEID::TERRAN_SIEGETANK));
     int count = 0;
+    // Count siege tanks within 2.0f of factory
     for (const auto &seige_tank : seige_tanks) {
         float distance = sc2::Distance2D(seige_tank->pos, factory->pos);
         if (distance < 2.0f) {
@@ -62,18 +81,31 @@ int BasicSc2Bot::CountNearbySeigeTanks(const sc2::Unit* factory) {
     return count;
 }
 
+/*
+ * Counts units of type unit_type
+ */
 size_t BasicSc2Bot::CountUnitType(sc2::UNIT_TYPEID unit_type) {
     return Observation()->GetUnits(sc2::Unit::Alliance::Self, IsUnit(unit_type)).size();
 }
 
+/*
+ * SCV Filter
+ */
 sc2::Filter scvFilter = [](const sc2::Unit& unit) {
     return unit.unit_type == sc2::UNIT_TYPEID::TERRAN_SCV;
 };
 
+/*
+ * @brief Finds nearest mineral patch to start
+ *
+ * @param start
+ * @return const Unit*
+ */
 const sc2::Unit* BasicSc2Bot::FindNearestMineralPatch(const sc2::Point2D& start) {
     sc2::Units units = Observation()->GetUnits(sc2::Unit::Alliance::Neutral);
     float distance = std::numeric_limits<float>::max();
     const sc2::Unit* target = nullptr;
+    // Get closest
     for (const auto& u : units) {
         if (u->unit_type == sc2::UNIT_TYPEID::NEUTRAL_MINERALFIELD || u->unit_type == sc2::UNIT_TYPEID::NATURALMINERALS || u->unit_type == sc2::UNIT_TYPEID::MINERALCRYSTAL || u->unit_type == sc2::UNIT_TYPEID::NEUTRAL_RICHMINERALFIELD  ) {
             float d = sc2::DistanceSquared2D(u->pos, start);
@@ -86,6 +118,12 @@ const sc2::Unit* BasicSc2Bot::FindNearestMineralPatch(const sc2::Point2D& start)
     return target;
 }
 
+/*
+ * @brief Finds nearest vespene geyser to start
+ *
+ * @param start
+ * @return const Unit*
+ */
 const sc2::Unit* BasicSc2Bot::FindNearestVespeneGeyser(const sc2::Point2D& start) {
     sc2::Units units = Observation()->GetUnits(sc2::Unit::Alliance::Neutral);
     float distance = std::numeric_limits<float>::max();
@@ -102,6 +140,13 @@ const sc2::Unit* BasicSc2Bot::FindNearestVespeneGeyser(const sc2::Point2D& start
     return target;
 }
 
+/*
+ * @brief Finds nearest command center to start
+ *
+ * @param start
+ * @param not_start_location
+ * @return const Point2D
+ */
 const sc2::Point2D BasicSc2Bot::FindNearestCommandCenter(const sc2::Point2D& start, bool not_start_location) {
 
     sc2::Units bases = Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsTownHall());
@@ -129,6 +174,12 @@ const sc2::Point2D BasicSc2Bot::FindNearestCommandCenter(const sc2::Point2D& sta
     }
 }
 
+/*
+ * @brief Finds nearest refinery to start
+ *
+ * @param start
+ * @return Point2D
+ */
 const sc2::Point2D BasicSc2Bot::FindNearestRefinery(const sc2::Point2D& start) {
 
     sc2::Units refineries = Observation()->GetUnits(sc2::Unit::Self, sc2::IsUnit(sc2::UNIT_TYPEID::TERRAN_REFINERY));
@@ -153,8 +204,13 @@ const sc2::Point2D BasicSc2Bot::FindNearestRefinery(const sc2::Point2D& start) {
     }
 }
 
-
-// checks that marine is nearby atleast size other marines
+/*
+ * @brief Returns the size of a given marine cluster
+ *
+ * @param marine
+ * @param marines
+ * @return int: size of cluster
+ */
 int BasicSc2Bot::MarineClusterSize(const sc2::Unit* marine, const sc2::Units& marines) {
     int num_nearby_marines = 0;
     const float distance_threshold_sq = 25.0f;
@@ -173,6 +229,12 @@ int BasicSc2Bot::MarineClusterSize(const sc2::Unit* marine, const sc2::Units& ma
 
     return num_nearby_marines;
 }
+
+/*
+ * @brief Finds a marine whose health is not at max (for medivacs)
+ *
+ * @return Unit*
+ */
 const sc2::Unit* BasicSc2Bot::FindInjuredMarine() {
     const sc2::Units marines = Observation()->GetUnits(sc2::Unit::Alliance::Self, IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE));
     
@@ -193,6 +255,12 @@ const sc2::Unit* BasicSc2Bot::FindInjuredMarine() {
     }
     return nullptr;
 }
+/*
+ * @brief Sorts medivacs according to their distance to start
+ *
+ * @param start
+ * @return Units
+ */
 const sc2::Units BasicSc2Bot::SortMedivacsAccordingToDistance(const sc2::Point2D start) {
     sc2::Units medivacs = Observation()->GetUnits(sc2::Unit::Alliance::Self, IsUnit(sc2::UNIT_TYPEID::TERRAN_MEDIVAC));
     std::sort(medivacs.begin(), medivacs.end(), [&start](const sc2::Unit* unit_a, const sc2::Unit* unit_b) {
@@ -208,6 +276,15 @@ const sc2::Units BasicSc2Bot::SortMedivacsAccordingToDistance(const sc2::Point2D
         });
     return medivacs;
 }
+
+/*
+ * @brief Finds largest marine cluster to start
+ *
+ * @param start
+ * @param unit
+ * @return const Point2D(0, 0) if unseccessful
+ * @return Point2D
+ */
 const sc2::Point2D BasicSc2Bot::FindLargestMarineCluster(const sc2::Point2D& start, const sc2::Unit& unit) {
     const sc2::Units marines = Observation()->GetUnits(sc2::Unit::Alliance::Self, IsUnit(sc2::UNIT_TYPEID::TERRAN_MARINE));
     sc2::Units medivacs = SortMedivacsAccordingToDistance(start);
@@ -245,11 +322,13 @@ const sc2::Point2D BasicSc2Bot::FindLargestMarineCluster(const sc2::Point2D& sta
 }
 
 
-
 /*
- * Gets an SCV that is currently gathering, or return nullptr if there are none.
+ * @brief Gets an SCV that is currently gathering, or return nullptr if there are none.
  * Useful to call when you need to assign an SCV to do a task but you don't want to
  * interrupt other important tasks.
+ *
+ * @param starting_point
+ * @return const Unit*
  */
 const sc2::Unit *BasicSc2Bot::GetGatheringScv() {
     const sc2::Units &gathering_scv_units = Observation()->GetUnits(sc2::Unit::Alliance::Self, [](const sc2::Unit& unit) {
@@ -272,8 +351,8 @@ const sc2::Unit *BasicSc2Bot::GetGatheringScv() {
 }
 
 /*
-* Functor that implements < for a Point2D so that we can holds points in a std::set in FindPlaceablePositionNear
-*/
+ * @brief Functor that implements < for a Point2D so that we can holds points in a std::set in FindPlaceablePositionNear
+ */
 struct Point2DLt {
     bool operator()(const sc2::Point2D& lhs, const sc2::Point2D& rhs) const {
         if (lhs.x == rhs.x) {
@@ -282,18 +361,15 @@ struct Point2DLt {
         return lhs.x < rhs.x;
     }
 };
-
 /*
-* Finds a location to place a building at that is near a given starting position
-*/
+ * @brief Finds a location to place a building at using a triangle with its base as a line between 2 nearest structures to starting_point
+ *
+ * @param starting_point
+ * @param ability_to_place_building
+ * @return Point2D(0, 0) if unseccessful
+ * @return Point2D
+ */
 sc2::Point2D BasicSc2Bot::FindPlaceablePositionNear(const sc2::Point2D& starting_point, const sc2::ABILITY_ID& ability_to_place_building) {
-    /*
-    * We want to find a position that is near starting_point that works to fit the building
-    * Idea: search in a square around the starting point for the first available position. If no where
-    * is found, increase the size of the square and search again.
-    * - so you still build relatively close to the starting point and increase the search space gradually
-    * - though this does seem to be a bit slow sometimes
-    */
 
     // Get all existing structures
     sc2::Units all_buildings = Observation()->GetUnits(sc2::Unit::Alliance::Self, sc2::IsUnits({ sc2::UNIT_TYPEID::TERRAN_COMMANDCENTER,
@@ -315,6 +391,13 @@ sc2::Point2D BasicSc2Bot::FindPlaceablePositionNear(const sc2::Point2D& starting
         });
 
     // Start by using nearest structures
+
+
+    /*
+        This portion of the code was implemented with the help of ChatGPT
+        * via ChatGPT: "im trying to use triangles to nicely space out my structures from another, take 2 and make a triangle out of them using the line between the structures"
+        * "i want you to make sure it finds a point it has to keep finding one until the placement condition is true"
+    */
     for (int i = 0; i < all_buildings.size(); i++) {
         if ((i + 1) < all_buildings.size()) {
             const sc2::Unit* a = all_buildings[i];
@@ -367,7 +450,7 @@ sc2::Point2D BasicSc2Bot::FindPlaceablePositionNear(const sc2::Point2D& starting
                     // Reset height and continue searching
                     height_of_triangle = 5.0f;
 
-                    // Stop searching if we cannot find a point before threshold -> use old way
+                    // Stop searching if we cannot find a point before threshold -> return error point
                     if (direction_changes > 4) {
                         new_point = sc2::Point2D(0, 0); 
                         return new_point;
@@ -380,105 +463,6 @@ sc2::Point2D BasicSc2Bot::FindPlaceablePositionNear(const sc2::Point2D& starting
             }
         }
     }
-
-
-
-
-
-  
-    /*
-    * Starting lower & upper bounds for x
-    */
-    int x_lo = -5, x_hi = 5;
-    /*
-    * Starting lower & upper bounds for y
-    */
-    int y_lo = -5, y_hi = 5;
-
-    /*
-    * How much to change the lower & upper bounds if you don't find a suitable spot in the current search square
-    */
-    int x_step = 5;
-    int y_step = 5;
-    bool found_pos_to_place_at = false;
-
-    /*
-    * When leaving the loop, pos_to_place should be set
-    */
-    sc2::Point2D pos_to_place_at;
-
-    /*
-    * Use a cache to not repeatedly query positions we know don't work
-    */
-    std::set<sc2::Point2D, Point2DLt> searched_points = {};
-
-    size_t loop_count = 0;
-    while (!found_pos_to_place_at) {
-        for (int x = x_lo; x <= x_hi; x += 3) {
-            for (int y = y_lo; y <= y_hi; y += 3) {
-                const sc2::Point2D current_pos = starting_point + sc2::Point2D(x, y);
-                sc2::QueryInterface* query = Query();
-                if (searched_points.find(current_pos) != searched_points.end()) {
-                    continue;
-                }
-
-                const bool can_place_here = query->Placement(ability_to_place_building, current_pos);
-                if (!can_place_here) {
-                    searched_points.insert(current_pos);
-                    continue;
-                }
-
-                /*
-                * We need wiggle room left & right so that the buildings aren't packed and they have room to build addons
-                */
-                const bool can_wiggle_left = query->Placement(ability_to_place_building, sc2::Point2D(current_pos.x - 2, current_pos.y));
-                if (!can_wiggle_left) {
-                    searched_points.insert(current_pos);
-                    continue;
-                }
-                const bool can_wiggle_right = query->Placement(ability_to_place_building, sc2::Point2D(current_pos.x + 2, current_pos.y));
-                if (!can_wiggle_right) {
-                    searched_points.insert(current_pos);
-                    continue;
-                }
-                // we found a valid position to place at, don't iterate again
-                found_pos_to_place_at = true;
-                // TODO: Find a way to speed this up
-                // ensures we dont build at expansion location
-                bool is_expansion_location = false;
-                for (const auto& expansion_location : expansion_locations) {
-                    float distance = std::sqrt(std::pow(expansion_location.x - current_pos.x, 2) +
-                        std::pow(expansion_location.y - current_pos.y, 2));
-                    if (distance <= 10.f) {
-                        is_expansion_location = true;
-                        break;
-                    }
-                }
-
-                if (!is_expansion_location) {
-                    pos_to_place_at = current_pos;
-                }
-                
-            }
-        }
-        // increase the search space
-        x_lo -= x_step;
-        x_hi += x_step;
-        y_lo -= y_step;
-        x_hi += y_step;
-        
-        if (loop_count++ > 5) { // todo: change back to 10 (?)
-            std::cout << "LOTS OF LOOPS OOPS " << loop_count << std::endl;
-            return sc2::Point2D(0, 0);
-            /*
-            float rand_x = sc2::GetRandomScalar() * 5.0f;
-            float rand_y = sc2::GetRandomScalar() * 5.0f;
-            return this->FindPlaceablePositionNear(starting_point + sc2::Point2D(rand_x, rand_y), ability_to_place_building);
-            */
-        }
-        
-    }
-    return pos_to_place_at;
 }
 
 /**
